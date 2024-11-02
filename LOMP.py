b0169a458625a145697f4ee7a5c1bfe07863db89
liftonemillionpounds <156608323+liftonemillionpounds@users.noreply.github.com>
2024-11-01 22:05:43 -0400

    l

--- LOMP.py
+++ LOMP.py
@@ -8,13 +8,15 @@ from googleapiclient.errors import HttpError
 
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
+import pandas as pd
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = "1BOnpf3t9ldGz0tTWSXnjYyD0fghsq8RwOesOzj10Zwk"
-SAMPLE_RANGE_NAME = "B2:D"
+SAMPLE_RANGE_NAME = "A2:D"
 
 
 def main():
@@ -58,12 +60,25 @@ def main():
 
     names = []
     pounds = []
+    dates = []
     for row in values:
       try:# Print columns A and E, which correspond to indices 0 and 4.
-        names.append(row[0])
-        pounds.append(float(row[1]))
+        names.append(row[1])
+        pounds.append(float(row[2]))
+        dates.append(str(row[0]))
       except:
          continue
+    
+
+    ts = pd.Series(pd.to_datetime(dates[-1]))
+    doy = ts.dt.dayofyear[0]
+    if ts.dt.is_leap_year[0] == True:
+      diy = 366
+    else:
+      diy = 365
+    pace = 10**6*doy/diy
+    names.append("Pace")
+    pounds.append(pace)
 
     names = np.array(names)
     pounds = np.array(pounds)
@@ -74,23 +89,29 @@ def main():
         percentiles.append(np.sum(pounds[names == t]))
     percentiles = np.array(percentiles)
   
-    fig, ax1 = plt.subplots(figsize=(9, 6), layout='constrained')
+    fig, (ax1, ax3) = plt.subplots(2, 1, figsize=(9, 6), height_ratios = [5,1], layout='constrained')
     fig.canvas.manager.set_window_title('')
     rects = ax1.barh(test_names, percentiles, align='center', height=0.5)
     large_percentiles = []
     small_percentiles = []
+    numonpace = 0
+    numbehind = 0
     for p in percentiles:
         if p < 100000:
             large_percentiles.append('')
-            small_percentiles.append(p)
+            small_percentiles.append('{:.1f}'.format(p))
         else:
-            large_percentiles.append(p)
+            large_percentiles.append('{:.1f}'.format(p))
             small_percentiles.append('')
+        if p > pace:
+           numonpace += 1
+        if p < pace:
+           numbehind += 1
 
     ax1.bar_label(rects, small_percentiles,
                     padding=5, color='black', fontweight='bold')
     ax1.bar_label(rects, large_percentiles,
-                    padding=-50, color='white', fontweight='bold')
+                    padding=-55, color='white', fontweight='bold')
 
     ax1.set_xlim([0, 1000000])
 
@@ -121,6 +142,25 @@ def main():
         labels = (percent_labels))
 
     ax2.set_ylabel('Percentage Completion')
+    onpace = "{} people are ahead of schedule! ".format(numonpace)
+    behind = "{} people are behind schedule!".format(numbehind)
+    latest_person = names[-2]
+    latest_pounds = pounds[-2]
+    latest = "Latest Update: {} lifted {} pounds! Go {}!".format(latest_person, latest_pounds, latest_person)
+    
+    totalpeople = numbehind + numonpace
+
+    if totalpeople == numbehind:
+      everyonebehind = "Everyone is behind!"
+      ax3.text(0.5,0.7,everyonebehind, horizontalalignment = "center", fontsize = 16, color = "red")
+    elif totalpeople == numonpace:
+      everyoneahead = "Everyone is ahead of schedule!"
+      ax3.text(0.5,0.7,everyoneahead, horizontalalignment = "center", fontsize = 16, color = "green")
+    else:
+      ax3.text(0.25,0.7,onpace, horizontalalignment = "center", fontsize = 16, color = "green")
+      ax3.text(0.75,0.7,behind, horizontalalignment = "center", fontsize = 16, color = "red")
+    ax3.text(0.5,0.2,latest, horizontalalignment = "center", fontsize = 16)
+    ax3.axis('off')
     plt.savefig("LOMP.png", dpi = 300)
   except HttpError as err:
     print(err)
